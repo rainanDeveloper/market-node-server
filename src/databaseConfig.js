d1c0ad1aee868a3a4eaf8abfe2fe205837b71046
Rainan Miranda de Jesus <jeris.rainan@gmail.com>
2019-08-30 23:01:52 -0300

    Defined migrations diretory, created connection

---
+++ src/databaseConfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host: '127.0.0.1',
+  port: 	'3306',
+  user: 'myUser',
+  password: 'root',
+  database: 'bluetools'
+}
---
+++ src/dbCreate.js
@@ -0,0 +1,1 @@
+const db	= require('./database/migrations/index.js')

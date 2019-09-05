5220427ffafa8936d90620ad2d6540c9576082a8
Rainan Miranda de Jesus <jesus.rainan@gmail.com>
2019-09-04 21:34:39 -0300

    Database migration logic done

---
+++ src/database/migrations/04092019-venda.js
@@ -0,0 +1,6 @@
+module.exports = {
+	description	: "Creation of basic user's table",
+	sqlData 	: 'CREATE TABLE IF NOT EXISTS venda ('+
+	'   vend_cod INT NOT NULL,'+
+	'   PRIMARY KEY (vend_cod));'
+}
--- src/dbCreate.js
+++ src/dbCreate.js
@@ -1,1 +1,49 @@
-const db	= require('./database/migrations/index.js')
+// const db	= require('./database/')
+const fs 			= require('fs')
+const mysql			= require('mysql')
+const config		= require('./databaseConfig.js')
+const connection	= mysql.createConnection(config)
+
+function createDB(connection, sql) {
+	connection.query(sql, (error, results, fields)=>{
+		if (fields) {
+			console.log(fields)
+		}
+
+		if (results) {
+			console.log(results)
+		}
+
+		if (error) {
+			console.log(error)
+		}
+	})
+}
+
+let dir = './database/migrations/'
+
+
+
+
+connection.connect((callback)=>{
+	if (callback) {
+		return console.log("Conexão não pode ser estabelecida. Detalhes do erro: "+callback)
+	}
+	console.log("Conexão com mysql realizada com sucesso!")
+
+	let migrationsFiles = fs.readdirSync(dir)
+	let dbSql = ''
+	
+	migrationsFiles.forEach((element, index)=>{
+		if (element!='index.js' & !element.match(/^\./g)) {
+			migration = require(dir+(element.replace(/\.js$/g, '')))
+			
+			dbSql = migration.sqlData
+
+			createDB(connection, dbSql)
+			console.log(dbSql)
+		}
+	})
+})
+
+
